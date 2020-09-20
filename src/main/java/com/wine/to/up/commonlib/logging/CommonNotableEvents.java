a9236c9c625e13264deb21b0daefc8ed6e7c838c
Ivan Zelenkov <ivan_zelenkov@dell.com>
2020-09-20 23:56:13 +0300

    fix bpp

--- src/main/java/com/wine/to/up/commonlib/logging/CommonNotableEvents.java
+++ src/main/java/com/wine/to/up/commonlib/logging/CommonNotableEvents.java
@@ -1,14 +1,10 @@
 package com.wine.to.up.commonlib.logging;
 
 public enum CommonNotableEvents implements NotableEvent {
-    DEFAULT("Default event"),
-    EXCEPTION("Some exception occurred"),
-
     W_KAFKA_SEND_MESSAGE_FAILED("Kafka send message failed. Topic: {}"),
     F_KAFKA_CONSUMER_POLL_FAILED("Kafka consumer polling failed. Topic: {}"),
     W_KAFKA_LISTENER_INTERRUPTED("Listener thread has been interrupted! Consuming topic name: {}"),
-    W_EXECUTOR_SHUT_DOWN("Executor has been shut down. Name or id: {}")
-    ;
+    W_EXECUTOR_SHUT_DOWN("Executor has been shut down. Name or id: {}");
 
     private final String template;
 
