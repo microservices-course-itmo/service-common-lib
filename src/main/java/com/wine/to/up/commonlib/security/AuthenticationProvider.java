6591ccfcb929d97aab283fe7ebe8d5873d79ee61
brovinnick <brovinnick@gmail.com>
2020-11-27 19:10:17 +0300

    Code refactor

--- src/main/java/com/wine/to/up/commonlib/security/AuthenticationProvider.java
+++ src/main/java/com/wine/to/up/commonlib/security/AuthenticationProvider.java
@@ -3,11 +3,24 @@ package com.wine.to.up.commonlib.security;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-public class Auth {
+/**
+ * The AuthenticationProvider represents information about current user's session
+ *
+ * @see <a href="https://www.notion.so/097028bcc9004428b65852f13c3211af">Full documentation on notion.so</a>
+ */
+public class AuthenticationProvider {
+  /**
+   * Tests if current user is anonimous
+   * @return true if current user is anonimous
+   */
   public static boolean isAnonymous() {
     return getUser().getRole().equals(HeadersAuthFilter.ROLE_ANONYMOUS);
   }
 
+  /**
+   * Returns User entity of current user, logged in to the service
+   * @return currently logged in user's entity
+   */
   public static User getUser() {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     return (User) authentication.getPrincipal();
